d79075e38ab64916e6d934441af84ecf65f69b3a
Bruno Petinati <brunopetinati@hotmail.com>
2021-01-14 20:33:46 -0300

    same as

--- src/store/modules/login/thunk.js
+++ src/store/modules/login/thunk.js
@@ -1,10 +1,13 @@
 import api from "../../../services/api";
 import { userLogin } from "./actions";
 
-const loginThunk = (data) => async (dispatch) => {
+const loginThunk = (result) => async (dispatch) => {
   await api
-    .post("/login", { ...data })
-    .then((res) => dispatch(userLogin(res.data)))
+    .post("/login", result)
+    .then((res) =>
+      window.localStorage.setItem("authToken", res.data.accessToken)
+    )
+    .then((res) => dispatch(userLogin(res)))
     .catch((err) => console.log(err));
 };
 
