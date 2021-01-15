c9ded2eaecb6bd65ccbdb020cc7e0d4622bfdb82
Bruno Petinati <brunopetinati@hotmail.com>
2021-01-14 23:25:04 -0300

    pending messages

---
+++ src/pages/Login/style.jsx
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  align-items: center;
+  justify-content: center;
+  height: 75vh;
+  width: 100vw;
+  background-color: #a8dadc;
+  @media (min-width: 1000px) {
+    flex-direction: row;
+    flex-wrap: wrap;
+    width: 100vw;
+  }
+`;
+const HeaderMobile = styled.div`
+  background-color: #bb1e1e;
+  width: 100vw;
+  height: 110px;
+  display: flex;
+  justify-content: center;
+
+  @media (min-width: 1000px) {
+    display: none;
+  }
+`;
+
+const HeaderDesktop = styled.div`
+  display: flex;
+  justify-content: space-around;
+  padding: 10px;
+  width: 100vw;
+  @media (max-width: 1000px) {
+    display: none;
+  }
+`;
+const FormContainer = styled.div`
+  background-color: white;
+  width: 30vw;
+  height: 30vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+
+  @media (min-width: 1000px) {
+    width: 30vw;
+  }
+`;
+
+export { Container, HeaderMobile, HeaderDesktop, FormContainer };
