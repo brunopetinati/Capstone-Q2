d4b11b294ee63194f0a3a1f91ae576448fd698b1
Franciele <Franciele Cristine>
2021-01-17 11:51:00 -0300

    chore: students register with redux

--- src/pages/Register/Student/index.jsx
+++ src/pages/Register/Student/index.jsx
@@ -1,8 +1,34 @@
+import {useDispatch} from 'react-redux';
+import {registerStudentsThunk} from '../../../store/modules/students/thunk';
+import * as yup from 'yup';
+import {yupResolver} from '@hookform/resolvers/yup';
+import {useForm} from 'react-hook-form';
+
 const StudentRegister = () => {
+  const dispatch = useDispatch();
+  const schema = yup.object().shape({
+      name: yup.string().required("Campo Obrigatório"),
+      group: yup.string().required("Campo Obrigatório"),
+      postscript: yup.string()
+  })
+  const {register, handleSubmit, errors} = useForm({
+    resolver: yupResolver(schema)
+  })
+
+  const handleForm = (data) =>{
+    dispatch(registerStudentsThunk(data))
+  }
   return (
     <div>
-      Essa é a página em que o professor poderá cadastrar o aluno dentro de
-      determinada turma.(Cadastrar Aluno)
+      <form onSubmit={handleSubmit(handleForm)}>
+        <input name="name" placeholder="Nome" ref={register}/>
+        {errors.name && <div>{errors.name.message}</div>}
+        <input name="group" placeholder="Turma" ref={register}/>
+        {errors.group && <div>{errors.group.message}</div>}
+        <textarea name="postscript" placeholder="Observações" ref={register}/>
+        {errors.postscript && <div>{errors.postscript.message}</div>}
+        <button type="submit">cadastrar</button>
+      </form>
     </div>
   );
 };
