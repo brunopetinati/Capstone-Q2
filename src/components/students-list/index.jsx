93b7179c12280bdf4d885b8181d17d770282bb6d
Franciele <Franciele Cristine>
2021-01-18 13:14:53 -0300

    chore: list of students with checkbox.

--- src/components/students-list/index.jsx
+++ src/components/students-list/index.jsx
@@ -1,17 +1,30 @@
 import {useState, useEffect} from 'react';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+import {getStudentsThunk} from '../../store/modules/students/thunk'
 
 
 const StudentsList = () =>{ 
-  const students = useSelector(state => state.students);
-  console.log(students)
+  const dispatch = useDispatch();
+  const students = useSelector((state) => state.students);
+  const [selected, setSelected] = useState([]);
+
+  console.log(students);
+
+  useEffect(() => {
+    dispatch(getStudentsThunk());
+  }, [dispatch]);
+
+ 
+
     return(
         <div>
             <p>Alunos</p>
+            <input/>
+            <button>Buscar</button>
             <ul>
               {students.map(({name}, index) =>{
                 return(
-                  <li key={index}><input type="checkbox"/>{name}</li>
+                  <li key={index}><input type="checkbox" value={name} onChange={(e) => console.log(e.target.value)}/>{name}</li>
                 )
               })}
             </ul>
