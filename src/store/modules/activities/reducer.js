5ab122205d33928be567707af086f052ceb28625
Bruno Petinati <brunopetinati@hotmail.com>
2021-01-19 11:10:28 -0300

    style:button

--- src/store/modules/activities/reducer.js
+++ src/store/modules/activities/reducer.js
@@ -1,18 +1,13 @@
 import { ADD_ACTIVITY, LIST_ACTIVITY } from "./actions-types";
 
-const activitiesReducer = (state=[], actions) =>{
-    switch (actions.types) {
-        case ADD_ACTIVITY:
-            const {activity} = actions;
-            return state = [...state, activity];
-        case LIST_ACTIVITY:
-            const {list} = actions;
-            return list;
-    
-        default:
-            return state;
-    }
-}
+const activitiesReducer = (state = [], actions) => {
+  switch (actions.types) {
+    case ADD_ACTIVITY:
+      const { activity } = actions;
+      return (state = [...state, activity]);
+    case LIST_ACTIVITY:
+      const { list } = actions;
+      return list;
 
     default:
       return state;
