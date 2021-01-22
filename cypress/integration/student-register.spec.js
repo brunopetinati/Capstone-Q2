5ed2256d60fc6919d0c4c7aa025b89c05f547db9
Bruno Petinati <brunopetinati@hotmail.com>
2021-01-22 03:18:58 +0000

    Update student-register.spec.js

--- cypress/integration/student-register.spec.js
+++ cypress/integration/student-register.spec.js
@@ -1,6 +1,6 @@
 /* eslint-disable cypress/no-unnecessary-waiting */
 context("Students Page", () => {
-  it("logs in with valid credentials, then verifies if user is logged by going to the student's profile page. Then asks for ID 10 and verifies if student obtained is Charles Darwin", () => {
+  it("logs in with valid credentials, then verifies if user is logged by going checking the menu bar", () => {
     cy.viewport(1366, 768);
     cy.visit("http://localhost:3000/register");
     cy.contains("Login");
@@ -10,17 +10,13 @@ context("Students Page", () => {
     cy.get(":nth-child(5) > .sc-kEjbxe").type("123456");
     cy.get(":nth-child(6) > .sc-kEjbxe").type("123456");
     cy.get(".sc-fubCfw").click();
+    cy.wait(1000);
     cy.visit("http://localhost:3000/login");
     cy.get(":nth-child(1) > .sc-eCssSg").type("willbedeleted@hotmail.com");
     cy.get(":nth-child(3) > .sc-eCssSg").type("123456");
     cy.get(".sc-jSgupP").click();
-    cy.wait(1000);
-    cy.visit("http://localhost:3000/profile");
-    cy.wait(500);
-    cy.contains("Obter Informações");
-    cy.get("input").type("10");
-    cy.get(".sc-kLgntA > :nth-child(2)").click();
-    cy.wait(600);
-    cy.contains("Charles Darwin");
+    cy.visit("http://localhost:3000/students");
+    cy.wait(5000);
+    cy.contains("Minhas Atividades");
   });
 });
